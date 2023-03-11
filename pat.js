23fb36ddf0d885338710b5be0ce4a704c50ab354
Jordan Addleman <xibalbarising@gmail.com>
2023-03-11 11:58:46 -0500

    DRY up pat.js line width < 80 chars

--- pat.js
+++ pat.js
@@ -2,53 +2,52 @@ const fs = require('fs');
 const readline = require('readline');
 const process = require('process');
 
-// POJO to track all patients and actions
+// patients Object to track all patients and activity
 const patients = {};
 
+// readline interface with input set to 2nd argument of node call
 const rl = readline.createInterface({
   input: fs.createReadStream(process.argv[2])
 });
 
+// process each line of text input
 rl.on('line', (line) => {
   const [type, ...args] = line.split(' ');
   // add any new patients to POJO 'Patients' as a new object with a treatments array property
   if (type === 'Patient') {
-    !patients[args[0]] ? patients[args[0]] = {treatments: []} : null;
+    patients[args[0]] ? null : patients[args[0]] = {treatments: []};
   }
   // if patient has any actions
   if (type === 'Action') {
     const [action, name, timeStamp, code] = args;
-    // if intake
+    // if action is intake
     if (action === 'Intake') {
       patients[name].intake = new Date(timeStamp);
     }
-    // if treatment
+    // if action is treatment
     if (action === 'Treatment') {
       patients[name].treatments.push({code: new Date(timeStamp)});
     }
-    // if discharge
+    // if action is discharge
     if (action === 'Discharge') {
       patients[name].discharge = new Date(timeStamp);
     }
   }
-
 });
 
+// sort through patients object to format data for console log
 rl.on('close', () => {
   for (var patient in patients) {
-
-    // is discharged after intake?
-    if (patients[patient].discharge > patients[patient].intake) {
-      var totalTreatments = patients[patient].treatments.length;
-      var intake = patients[patient].intake;
-      var discharge = patients[patient].discharge;
+    var currPatient = patients[patient];
+    var { intake, discharge, treatments } = currPatient;
+    // if discharged after intake and received at least 1 treatment...
+    if (discharge > intake && treatments.length > 0) {
       var stayLength = discharge.getTime() - intake.getTime();
       var hours = stayLength / (1000 * 60 * 60);
       var minutes = Math.round((hours % 1) * 60).toFixed(1);
       hours = Math.floor(hours).toFixed(1);
-      console.log(`Patient ${patient} stayed for ${hours} hours and ${minutes} minutes and received ${totalTreatments} treatments`);
+      console.log(`Patient ${patient} stayed for ${hours} hours`,
+      `and ${minutes} minutes and received ${treatments.length} treatments`);
     }
   }
-
 });
-
