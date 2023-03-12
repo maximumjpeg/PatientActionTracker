fcf17bcd6e54a5219d127554de399b3398f5c286
Jordan Addleman <xibalbarising@gmail.com>
2023-03-11 21:40:47 -0500

    Create index.test.js
    add test file

---
+++ index.test.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const readline = require('readline');
+const { trackPatients } = require('./index.js');
+
+describe('trackPatients', () => {
+  test('should log an error message if no input file is detected', () => {
+    const consoleSpy = jest.spyOn(console, 'log');
+    process.argv[2] = undefined;
+    trackPatients();
+    expect(consoleSpy).toHaveBeenCalledWith('ERROR: No .txt input file detected!');
+    consoleSpy.mockReset();
+  });
+
+  test('should only log patient data if intake, discharge and 1 or more treatments', (done) => {
+    const consoleSpy = jest.spyOn(console, 'log');
+    process.argv[2] = 'testExample1.txt';
+    trackPatients();
+    setTimeout(() => {
+      expect(consoleSpy).toHaveBeenCalledWith('Patient John stayed for 222.0 hours and 13.0 minutes and received 4 treatments');
+      done();
+      consoleSpy.mockReset();
+    }, 100);
+  });
+
+  // test('should log all complete patient data, even if the data is not in chronological order', () => {
+  //   const consoleSpy = jest.spyOn(console, 'log');
+  //   process.argv[2] = 'testExample2.txt';
+  //   trackPatients();
+  //   setTimeout(() => {
+  //     expect(consoleSpy).toHaveBeenCalledWith('');
+  //     done();
+  //     consoleSpy.mockReset();
+  //   }, 100);
+  // });
+});
